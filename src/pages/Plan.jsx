f6f4c0e8a2edac06017fcc10e457217d053c470d
Brandon Kim <kimbrandon97@gmail.com>
2024-02-17 01:29:01 -0800

    change plan and pricing css

--- src/pages/Plan.jsx
+++ src/pages/Plan.jsx
@@ -83,7 +83,7 @@ const Plan = () => {
     return (
         <div>
             <Header />
-            <div className="text-3xl  mb-10">Plan & Pricing</div>
+            <div className="text-3xl mb-10 text-center md:text-start">Plan & Pricing</div>
             <div className="flex flex-col md:flex-row gap-4">
                 <div className="w-full md:w-1/3">
                     <PlanChoice 
